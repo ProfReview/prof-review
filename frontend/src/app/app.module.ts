057dfae35865434517739ccfe032abfc1e3099ce
tura-lab <tiruzertsedeke25@gmail.com>
2023-04-19 09:25:49 +0300

    instructor card done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { InstructorReviewComponent } from './components/instructor-review/instructor-review.component';
-import { TopTagsComponent } from './components/top-tags/top-tags.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -12,9 +10,9 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './components/login/login.component';
+import { InstructorReviewComponent } from './components/instructor-review/instructor-review.component';
 
-// Angular material stuff
-import { MatChipsModule } from '@angular/material/chips';
+// Angular-material stuff
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -25,15 +23,18 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
-import { InstructorReviewDisplayComponent } from './components/instructor-review-display/instructor-review-display.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { TopTagsComponent } from './components/top-tags/top-tags.component';
+import { ProfessorDetailComponent } from './components/professor-detail/professor-detail.component';
 
 @NgModule({
   imports: [
+    BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    BrowserModule,
 
     // Angular material
     MatFormFieldModule,
@@ -41,22 +42,22 @@ import { InstructorReviewDisplayComponent } from './components/instructor-review
     MatInputModule,
     MatSelectModule,
     MatIconModule,
-    MatChipsModule,
-
     MatRadioModule,
     MatGridListModule,
     MatToolbarModule,
     MatMenuModule,
     MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
   ],
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
     NavBarComponent,
-    InstructorReviewDisplayComponent,
-    TopTagsComponent,
     InstructorReviewComponent,
+    TopTagsComponent,
+    ProfessorDetailComponent
   ],
   providers: [],
   bootstrap: [AppComponent],
