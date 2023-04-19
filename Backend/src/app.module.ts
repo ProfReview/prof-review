8eee4bc99bfd0411f91e06d9f71056394dfd4973
amanuel <amanuelyihunie27@gmail.com>
2023-04-19 09:26:51 +0300

    Backend created ratings module

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'RateTheInstructor',
+      dbName: 'ProfReview',
     }),
     ReportsModule,
+    RatingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
